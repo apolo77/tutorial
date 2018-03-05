41cb6c2b35b4cb50ba58b285ff5ccee693faaa34
Apolo77 <juanjogaji@gmail.com>
2018-03-05 07:40:57 -0500

    solucion de acordeon preguntas frecuentas

--- javascript.js
+++ javascript.js
@@ -2,9 +2,18 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+/*
+var panel = this.nextElementSibling;
     if (panel.style.maxHeight){
       panel.style.maxHeight = null;
     } else {
@@ -12,3 +21,4 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+*/
